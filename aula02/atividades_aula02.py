5bd29f2f64145c8a180e20a887f4a386ddcc4313
abreuadrian <abreuadrianoliv@gmail.com>
2026-04-02 21:24:31 -0300

    Acrescimo Atividade 04 e Atividade 05

--- aula02/atividades_aula02.py
+++ aula02/atividades_aula02.py
@@ -1,22 +1,40 @@
-#Atividade 01 ------------------------------------------------------------------------------------------------------------------------------------------------------------------
-num = int(input('Digite um número: '))
-ant = num - 1 
-pos = num + 1
+# #Atividade 01 ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# num = int(input('Digite um número: '))
+# ant = num - 1 
+# pos = num + 1
 
-print(f'\nO número é {num}, o número anterior é {ant} e o seu posterior é {pos}.')
+# print(f'\nO número é {num}, o número anterior é {ant} e o seu posterior é {pos}.')
 
-#Atividade 02 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
-preco = float(input('Informe o valor do produto: '))
-qnt = int(input('Informe a quantidade: '))
-preco_tot = preco * qnt
-desc = preco_tot * 0.1
-preco_final = preco_tot - desc
+# #Atividade 02 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# preco = float(input('Informe o valor do produto: '))
+# qnt = int(input('Informe a quantidade: '))
+# preco_tot = preco * qnt
+# desc = preco_tot * 0.1
+# preco_final = preco_tot - desc
 
-print(f'\nO preço total da compra ficou em R${preco_tot}\nO valor com 10% de desconto (R${desc}) ficou em R${preco_final}')
+# print(f'\nO preço total da compra ficou em R${preco_tot}\nO valor com 10% de desconto (R${desc}) ficou em R${preco_final}')
 
-#Atividade 03 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
-salario = float(input('Digite seu salário: '))
-aumt = salario * 0.15
-salario_tot = salario + aumt
+# #Atividade 03 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# salario = float(input('Digite seu salário: '))
+# aumt = salario * 0.15
+# salario_tot = salario + aumt
 
-print(f'\nSalário: R${salario:.2f}.\nBônus do mês: R${aumt:.2f}.\nSalário final: R${salario_tot:.2f}.')
+# print(f'\nSalário: R${salario:.2f}.\nBônus do mês: R${aumt:.2f}.\nSalário final: R${salario_tot:.2f}.')
+
+# #Atividade 04 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# n = int(input('Informe um número: '))
+# dobro = n * 2
+# triplo = n * 3 
+# quad = n ** 2
+
+# print (f'O numero: {n}\nO dobro: {dobro}\nO triplo: {triplo}\nO quadrado: {quad}')
+
+#Atividade 05 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+nota1 = float(input('Informe a nota da AV1: '))
+nota2 = float(input('Informe a nota da AV2: '))
+nota3 = float(input('Informe a nota da AV3: '))
+nota4 = float(input('Informe a nota da AV4: '))
+
+med_final = (nota1 + nota2 + nota3 + nota4) / 4
+print(f'A média do aluno foi: {med_final:.1f}')
