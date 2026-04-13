6c1f704ab3671e2260817f6226947498c401376a
Adrian Oliveira <abreuadrianoliv@gmail.com>
2026-04-13 10:55:35 -0300

    acrescenta loop 'for' e lista em atividadespt3.py

--- aula02/atividadespt3.py
+++ aula02/atividadespt3.py
@@ -1,7 +1,15 @@
-nota1 = float(input('Informe a nota da AV1: '))
-nota2 = float(input('Informe a nota da AV2: '))
-nota3 = float(input('Informe a nota da AV3: '))
-nota4 = float(input('Informe a nota da AV4: '))
+#Atividade3:
+list_nota = []
+for i in range (4):
+    nota = float(input(f'Informe a nota da {i+1}ª avaliação: '))
+    list_nota.append(nota)
+med = sum(list_nota) / len(list_nota)
+print(f'\nA média do aluno foi: {med:.1f}')
 
-med_final = (nota1 + nota2 + nota3 + nota4) / 4
-print(f'A média do aluno foi: {med_final:.1f}')
+# nota1 = float(input('Informe a nota da AV1: '))
+# nota2 = float(input('Informe a nota da AV2: '))
+# nota3 = float(input('Informe a nota da AV3: '))
+# nota4 = float(input('Informe a nota da AV4: '))
+
+# med_final = (nota1 + nota2 + nota3 + nota4) / 4
+# print(f'A média do aluno foi: {med_final:.1f}')
